ff91d636ec7482c373f95854fd797903faa2f612
Kevin Hudson <darklen14@gmail.com>
2020-10-16 21:21:27 -0400

    ++CrustType index, upsert, controller, js

---
+++ PizzaPros/PizzaPros/Controllers/PizzaCrustTypeController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PizzaPros.DataAccess.Data.Repository.IRepository;
+
+namespace PizzaPros.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PizzaCrustTypeController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PizzaCrustTypeController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Json(new { data = _unitOfWork.PizzaCrustType.GetAll() });
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+
+            var objFromDb = _unitOfWork.PizzaCrustType.GetFirstOrDefault(u => u.Id == id);
+            if (objFromDb == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+                _unitOfWork.PizzaCrustType.Remove(objFromDb);
+                _unitOfWork.Save();
+                return Json(new { success = true, message = "Delete successful" });
+        }
+
+    }
+}
---
+++ PizzaPros/PizzaPros/Pages/Admin/CrustType/Upsert.cshtml.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using PizzaPros.DataAccess.Data.Repository.IRepository;
+
+namespace PizzaPros.Pages.Admin.CrustType
+{
+    public class UpsertModel : PageModel
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public UpsertModel(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        [BindProperty]
+        public Models.PizzaCrustType PizzaCrustTypeObj { get; set; }
+
+
+        public IActionResult OnGet(int? id)
+        {
+            PizzaCrustTypeObj = new Models.PizzaCrustType();
+            if (id != null)
+            {
+                PizzaCrustTypeObj = _unitOfWork.PizzaCrustType.GetFirstOrDefault(p => p.Id == id);
+                if (PizzaCrustTypeObj == null)
+                {
+                    return NotFound();
+                }
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            if (PizzaCrustTypeObj.Id == 0)
+            {
+                _unitOfWork.PizzaCrustType.Add(PizzaCrustTypeObj);
+            }
+            else
+            {
+                _unitOfWork.PizzaCrustType.Update(PizzaCrustTypeObj);
+            }
+            _unitOfWork.Save();
+            return RedirectToPage("./Index");
+        }
+    }
+}
