0f17402f2f86095ca4f0a0a73a3e309a30d9c8a7
Kevin Hudson <darklen14@gmail.com>
2020-10-27 17:14:59 -0400

    updated phone number validation

--- PizzaPros/PizzaPros/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ PizzaPros/PizzaPros/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -72,7 +72,7 @@ namespace PizzaPros.Areas.Identity.Pages.Account
             public string LastName { get; set; }
 
             [Required(ErrorMessage = "You must provide a phone number")]
-            [MaxLength(10)]
+            [MaxLength(14)]
             [DataType(DataType.PhoneNumber)]
             [RegularExpression(@"^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$", ErrorMessage = "Not a valid phone number")]
             [Display(Name = "Home Phone")]
@@ -94,12 +94,16 @@ namespace PizzaPros.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser 
-                { UserName = Input.Email, 
-                  Email = Input.Email,
-                  FirstName = Input.FirstName,
-                  LastName = Input.LastName,
-                  PhoneNumber = Input.PhoneNumber
+                var user = new ApplicationUser
+                { UserName = Input.Email,
+                    Email = Input.Email,
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    PhoneNumber = Input.PhoneNumber
+                                        .Replace(" ", "")
+                                        .Replace("(","")
+                                        .Replace(")","")
+                                        .Replace("-","")
                 };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if(!await _roleManager.RoleExistsAsync(SD.ManagerRole))
