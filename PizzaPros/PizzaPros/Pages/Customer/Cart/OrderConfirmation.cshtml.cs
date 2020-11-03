d2adab51df1f75422847bb8a833217b5264eed72
Kevin Hudson <darklen14@gmail.com>
2020-11-03 15:19:00 -0500

    ++orderConfirmation page and model

---
+++ PizzaPros/PizzaPros/Pages/Customer/Cart/OrderConfirmation.cshtml.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PizzaPros.Pages.Customer.Cart
+{
+    public class OrderConfirmationModel : PageModel
+    {
+        [BindProperty] // from summary post() handler id
+        public int orderId { get; set; }
+        public void OnGet(int id )
+        {
+            orderId = id;
+        }
+    }
+}
