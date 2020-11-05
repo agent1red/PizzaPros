384a7077f6836f733954f8f0bb0474ade02bca37
Kevin Hudson <darklen14@gmail.com>
2020-11-05 17:37:37 -0500

    ++countTimer for Manage Order, Navbar fixed-top

---
+++ PizzaPros/PizzaPros/wwwroot/js/countTimer.js
@@ -0,0 +1,31 @@
+﻿
+
+var exp = document.getElementsByClassName('exp1');
+x = document.getElementsByClassName("timer");
+countdown();
+function countdown() {
+    var now = new Date();
+    for (i = 0; i < exp.length; i++) {
+        var e = new Date(exp[i].innerHTML);
+        var distance = e - now;
+        var timeDiff = e.getTime() - now.getTime();
+        var seconds = Math.floor(timeDiff / 1000);
+        var minutes = Math.floor(seconds / 60);
+        var hours = Math.floor(minutes / 60);
+        var days = Math.floor(hours / 24);
+        hours %= 24;
+        minutes %= 60;
+        seconds %= 60;
+
+        if (distance < 0) {
+            clearInterval(countdown);
+            x[i].className = "col-12 timer text-center text-wrap font-weight-bold mb-4"
+            x[i].innerHTML = "<span style='color: red;'>Time Expired </span>"
+
+        } else {
+            x[i].innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s";
+        }
+    }
+
+}
+setInterval(function () { countdown();; }, 1000);
